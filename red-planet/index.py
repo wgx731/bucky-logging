6dfbb0c18eb5fb503df624ea9760180f3caf153b
wgx731 <wgx731@gmail.com>
2017-04-23 22:29:53 +0800

    feat[red]: finish red planet

---
+++ red-planet/index.py
@@ -0,0 +1,36 @@
+import logging, uuid
+from logging.handlers import RotatingFileHandler
+from flask import Flask
+
+
+LOG_FORMAT = '[%(asctime)s %(pathname)s:%(lineno)d] %(levelname)s - %(message)s'
+LOG_FILE = 'app.log'
+HOST = '0.0.0.0'
+PORT = 8080
+
+app = Flask(__name__)
+
+@app.route("/red")
+def red():
+    app.logger.info('id: %s, %s' %(uuid.uuid1(), {
+        'path': '/red'
+    }))
+    return 'deadeye-duck'
+
+@app.route("/error")
+def error():
+    app.logger.error('id: %s, %s' %(uuid.uuid1(), {
+        'path': '/error'
+    }))
+    return 'error'
+
+if __name__ == "__main__":
+    print('server run at %d' % PORT)
+    formatter = logging.Formatter(LOG_FORMAT)
+    handler = RotatingFileHandler(LOG_FILE, maxBytes=10000000,
+            backupCount=5)
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(handler)
+    app.logger.setLevel(logging.DEBUG)
+    app.run(host=HOST, port=PORT)
